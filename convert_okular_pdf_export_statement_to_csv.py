3df2d4aebf50d2b272606f5a081012aa146900d1
Perry Hartono <perryox@hotmail.com>
2024-10-26 22:59:25 +1100

    Account for 'closing balance' line at the end

--- convert_okular_pdf_export_statement_to_csv.py
+++ convert_okular_pdf_export_statement_to_csv.py
@@ -11,6 +11,7 @@ REGEX_TRANSACTION_WITH_BALANCE = REGEX_TRANSACTION + r'\s+' + REGEX_AMOUNT
 # Full regex: ([0-9]{2}-[A-Z][a-z]{2})\s+([0-9]{2}-[A-Z][a-z]{2})\s{2,}(.+)\s{2,}([0-9.,-]+)\s{2,}([0-9.,-]+)
 # For matching just the dates: ([0-9]{2}-[A-Z][a-z]{2})\s+([0-9]{2}-[A-Z][a-z]{2})
 
+REGEX_CLOSING_BALANCE = r'(Closing Balance)\s+' + REGEX_AMOUNT
 REGEX_END_OF_PAGE = r'(Page|Statement continues over|Bank of Queensland|Please check your|Remember to retain|mebank)'
 
 class Transaction:
@@ -30,31 +31,38 @@ def convert_input_into_transactions_list(file_to_load):
     transactions_list = []
 
     for input_line in input_data:
+        is_closing_balance = re.search(REGEX_CLOSING_BALANCE, input_line) != None
         is_new_transaction_with_balance = re.search(REGEX_TRANSACTION_WITH_BALANCE, input_line) != None
         is_new_transaction = re.search(REGEX_TRANSACTION, input_line) != None
         is_end_of_page = re.search(REGEX_END_OF_PAGE, input_line) != None
         is_reading_transaction = curr_transaction != None
         #print('[line] isNewTransaction={} isEndOfPage={} isReadingTransaction={} "{}"'.format(is_new_transaction, is_end_of_page, is_reading_transaction, input_line))
 
-        if is_new_transaction:
+        if is_closing_balance:
             if is_reading_transaction:
                 transactions_list.append(curr_transaction)
+            parsed_input = re.search(REGEX_CLOSING_BALANCE, input_line).groups()
+            curr_transaction = Transaction('', '', parsed_input[0], '0.00', parsed_input[1])
 
+        elif is_new_transaction:
+            if is_reading_transaction:
+                transactions_list.append(curr_transaction)
             transaction_regex = REGEX_TRANSACTION_WITH_BALANCE if is_new_transaction_with_balance else REGEX_TRANSACTION
-
             parsed_input = re.search(transaction_regex, input_line).groups()
             curr_transaction = Transaction(
                     parsed_input[0], parsed_input[1],
                     parsed_input[2], parsed_input[3],
                     parsed_input[4] if is_new_transaction_with_balance else '' 
             )
-            print(str(curr_transaction))
+            #print(str(curr_transaction))
+
         elif is_reading_transaction:
             if is_end_of_page:
                 transactions_list.append(curr_transaction)
                 curr_transaction = None
                 continue
             curr_transaction.description += ' ' + input_line.strip().replace(',', ' ')
+
         else:
             curr_transaction = None
             continue
